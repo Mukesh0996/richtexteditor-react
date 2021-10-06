03179be3a16ea1aad3001193894bab256d36a891
Mukesh <mukeshkeerthipati96@gmail.com>
2021-10-06 16:58:36 +0530

    updated UI elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
 
+    const [showOptions, setShowOptions] = useState(true);
+  
   const clickHandler = (event) => {
-    event.preventDefault();
-    const elem = event.target.getAttribute("data-icon");
-    console.log(elem);
-    document.execCommand(elem);
+      let command = event.target.getAttribute("data-icon");
+      document.execCommand(command, false, null);
+  }
+
+  const toggleAlignmentHandler = (event) => {
+      const alignment_options = document.querySelector(".alignment_options");
+      setShowOptions(!showOptions);
+      if(showOptions) {
+          alignment_options.classList.remove("hide");
+          setTimeout(()=> {
+            alignment_options.classList.add("show");
+          },200);
+      } else {
+          alignment_options.classList.remove("show");
+          setTimeout(()=> {
+            alignment_options.classList.add("hide");
+          },200)
+      }
+        
+  }
+
+  const toggleFontSizeHandler = () => {
 
   }
 
@@ -15,12 +35,19 @@ const App = () => {
   return <React.Fragment>
               <header className="header">Rich Text Editor</header>
               <div className="editor-container">
-              <div className="richTextOptions">
-              <div className="icon" data-icon="bold" onClick={clickHandler} > <i className='fas fa-bold' data-icon="bold"></i></div>
-              <div className="icon"> <i className='fas fa-italic'></i></div>
-             
-              </div>
-              <div className="editor" contentEditable={true}></div>
+                    <div className="richTextOptions">
+                        <i className='fas fa-bold' data-icon="bold" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i> 
+                        <i className='fas fa-italic' data-icon="italic" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>  
+                        <div className="alignment" onClick={toggleAlignmentHandler} onMouseDown={(e) => e.preventDefault()}>  
+                            <i className="fas fa-align-justify"></i>
+                            <div className="alignment_options hide">
+                                <i className="fas fa-align-left" data-icon="justifyLeft" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                                <i className="fas fa-align-justify" data-icon="justifyFull" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                                <i className="fas fa-align-right" data-icon="justifyRight" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="editor" contentEditable={true}></div>
               </div>
           </React.Fragment>
 
