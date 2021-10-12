367af6b2e9e03433b80262041f11f8daaa2afab8
Mukesh <mukeshkeerthipati96@gmail.com>
2021-10-12 13:34:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 const App = () => {
 
-    const [showOptions, setShowOptions] = useState(true);
-  
+   
+   
   const clickHandler = (event) => {
       let command = event.target.getAttribute("data-icon");
-      document.execCommand(command, false, null);
+      let value = event.target.getAttribute("data-value")+ "px";
+      console.log(command, value.toString());
+      document.execCommand(command, false, value.toString());
   }
 
-  const toggleAlignmentHandler = (event) => {
-      const alignment_options = document.querySelector(".alignment_options");
-      setShowOptions(!showOptions);
-      if(showOptions) {
-          alignment_options.classList.remove("hide");
+  const toggleHandler = (event) => {
+      const options = document.getElementById(`${event.currentTarget.getAttribute("id")}_options`);     
+      if(options.classList.contains("hide")) {
+        options.classList.remove("hide");
           setTimeout(()=> {
-            alignment_options.classList.add("show");
+            options.classList.add("show");
           },200);
-      } else {
-          alignment_options.classList.remove("show");
+
+      } else if(options.classList.contains("show")) {
+        options.classList.remove("show");
           setTimeout(()=> {
-            alignment_options.classList.add("hide");
-          },200)
+            options.classList.add("hide");
+          },200);
       }
-        
   }
 
   const toggleFontSizeHandler = () => {
 
   }
 
-
   return <React.Fragment>
               <header className="header">Rich Text Editor</header>
               <div className="editor-container">
                     <div className="richTextOptions">
-                        <i className='fas fa-bold' data-icon="bold" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i> 
-                        <i className='fas fa-italic' data-icon="italic" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>  
-                        <div className="alignment" onClick={toggleAlignmentHandler} onMouseDown={(e) => e.preventDefault()}>  
+                        <i className='fas fa-bold' data-icon="bold" data-value="" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i> 
+                        <i className='fas fa-italic' data-icon="italic" data-value="" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>  
+                        <div  id="alignment" className="alignment" onClick={toggleHandler} onMouseDown={(e) => e.preventDefault()}>  
                             <i className="fas fa-align-justify"></i>
-                            <div className="alignment_options hide">
-                                <i className="fas fa-align-left" data-icon="justifyLeft" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
-                                <i className="fas fa-align-justify" data-icon="justifyFull" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
-                                <i className="fas fa-align-right" data-icon="justifyRight" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                            <div id="alignment_options" className="alignment_options hide">
+                                <i className="fas fa-align-left" data-value="" data-icon="justifyLeft" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                                <i className="fas fa-align-justify" data-value="" data-icon="justifyFull" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                                <i className="fas fa-align-right" data-value="" data-icon="justifyRight" onClick={clickHandler} onMouseDown={(e) => e.preventDefault()}></i>
+                            </div>
+                        </div>
+                        <div  id="fontsize" className="fontsize" onClick={toggleHandler} onMouseDown={(e) => e.preventDefault()}>  
+                            <i className="fas">10</i>
+                            <div id="fontsize_options" className="fontsize_options hide">
+                                <i  className="fas" data-icon="fontSize" data-value="1" onClick={clickHandler}>10</i>
+                                <i className="fas" data-icon="fontSize" data-value="2" onClick={clickHandler}>11</i>
+                                <i className="fas" data-icon="fontSize" data-value="3" onClick={clickHandler}>12</i>
+                                <i className="fas" data-icon="fontSize" data-value="4" onClick={clickHandler}>13</i>
+                                <i className="fas" data-icon="fontSize" data-value="5" onClick={clickHandler}>14</i>
+                                <i className="fas" data-icon="fontSize" data-value="6" onClick={clickHandler}>15</i>
+                                <i className="fas" data-icon="fontSize" data-value="7" onClick={clickHandler}>16</i>
                             </div>
                         </div>
                     </div>
